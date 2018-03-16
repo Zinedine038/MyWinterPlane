7f7dcd66d796e2ac22479507856207aee7d2264f
Zinedine038 <zsghari@hotmail.com>
2018-03-16 15:27:22 +0100

    commiten die shit

---
+++ Assets/Code/PlayerManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerManager : MonoBehaviour
+{
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.transform.tag == "VehicleEnter")
+        {
+            if (Input.GetKeyDown(KeyCode.Return))
+            {
+                print("does");
+                other.transform.GetComponent<Vehicle>().Enter();
+            }
+        }
+    }
+}
--- Assets/Standard Assets/Characters/FirstPersonCharacter/Scripts/FirstPersonController.cs
+++ Assets/Standard Assets/Characters/FirstPersonCharacter/Scripts/FirstPersonController.cs
@@ -255,5 +255,7 @@ namespace UnityStandardAssets.Characters.FirstPerson
             }
             body.AddForceAtPosition(m_CharacterController.velocity*0.1f, hit.point, ForceMode.Impulse);
         }
+
+
     }
 }
