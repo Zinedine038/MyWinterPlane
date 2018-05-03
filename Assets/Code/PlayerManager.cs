5403c5971315012d98b784853f68902d256de703
Zinedine038 <zsghari@hotmail.com>
2018-05-03 13:11:38 +0200

    Some stuff

--- Assets/Code/PlayerManager.cs
+++ Assets/Code/PlayerManager.cs
@@ -1,16 +1,39 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerManager : MonoBehaviour
 {
     public int money;
+    public float hunger;
+    public float thrist;
+    public float coldness;
+
 
     public static PlayerManager instance;
+
+
+
+    public Text moneyText;
+    public Image hungerBar;
+    public Image thristBar;
+    public Image coldnessBar;
 
     private void Awake()
     {
         instance=this;
+        UpdateUI();
+        StartCoroutine("GraduallyDecreaseStats");
+    }
+
+
+    public void UpdateUI()
+    {
+        moneyText.text=money.ToString();
+        hungerBar.fillAmount = (hunger/100f);
+        thristBar.fillAmount = (thrist/100f);
+        coldnessBar.fillAmount = (coldness/100f);
     }
 
     private void OnTriggerStay(Collider other)
@@ -19,9 +42,22 @@ public class PlayerManager : MonoBehaviour
         {
             if (Input.GetKeyDown(KeyCode.Return))
             {
-                print("does");
                 other.transform.GetComponent<Vehicle>().Enter();
             }
+        }
+    }
+
+
+    IEnumerator GraduallyDecreaseStats()
+    {
+        while(true)
+        {
+            yield return new WaitForSeconds(1f);
+            thrist-=0.05f;
+            hunger-=0.0125f;
+            coldness-=0.025f;
+            UpdateUI();
+            yield return null;
         }
     }
 }
